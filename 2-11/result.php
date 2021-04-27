67bf3176a74655626d3cecb6b6d13441c725b1ed
miyakeiko <79643269+miyakeiko@users.noreply.github.com>
2021-04-27 19:32:17 +0900

    フォームに入力した内容を取得する 課題提出
    課題提出

---
+++ 2-11/result.php
@@ -0,0 +1,7 @@
+<?php
+$my_name = $_POST['my_name'];
+$password = $_POST['password'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>私のメールアドレスは、<?php echo $mail; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
