10930d81c85095371e8d65cb776729f6dccf0011
miyakeiko <79643269+miyakeiko@users.noreply.github.com>
2021-04-28 12:03:05 +0900

    いろいろなフォームの入力欄 課題提出
    課題提出

---
+++ 2-12/result12.php
@@ -0,0 +1,10 @@
+<?php
+    $my_name = $_POST['my_name'];
+    $award = $_POST['award'];
+    $number = $_POST['number'];
+
+    ?>
+    <p>お名前：<?php echo $my_name; ?></p>
+    <p>ご希望景品<?php echo $award; ?></p>
+    <p>個数：<?php echo $number; ?></p>
+    
