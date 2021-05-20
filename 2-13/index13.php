03abe39d774dbf7c8f62a494eefd5fa7234aa8cc
miyakeiko <79643269+miyakeiko@users.noreply.github.com>
2021-05-20 20:13:28 +0900

    便利な関数（配列）課題提出
    課題提出

---
+++ 2-13/index13.php
@@ -0,0 +1,69 @@
+<?php
+    $x=4.7;
+    echo ceil ($x);
+ 
+    echo '<br>';
+    $x=0.9;
+    echo floor($x);
+
+    echo '<br>';
+    $x = 0.9;
+    echo round($x);
+
+    
+    echo '<br>';
+    echo pi();
+    function circleArea($r) {
+        $circle_area = $r * $r * pi();
+        echo $circle_area; 
+    }
+    circleArea(150);
+
+    
+    echo '<br>';
+    echo mt_rand(1, 1000);
+
+    echo '<br>';
+    $str = "apple";
+    echo strlen($str);
+
+    echo '<br>';
+    $str = "apple";
+    echo strpos($str, "a");
+
+    echo '<br>';
+    $str = "apple";
+    echo substr($str, -3,3);
+    
+    echo '<br>';
+    $str = "apple";
+    echo str_replace("p", "P", $str);
+
+    echo '<br>';
+    $name = "太郎さん";
+    $limit_number = 10;
+    echo $name."の残り時間はあと".$limit_number."です";
+    echo '<br>';
+    printf("%sの残り時間はあと%dです", $name, $limit_number);
+
+
+    echo '<br>';
+    $name = "太郎"; $limit_time = 10; $price = 10000; printf("%sはあと%d時間で%dの罰金です。", $name, $limit_time, $price);
+
+    echo '<br>';
+    $limit_hour = 2;
+    $limit_minute = 15;
+    printf("残り%02d時間%02d分", $limit_hour, $limit_minute);
+    echo '<br>';
+    $limit_hour = 5;
+    $limit_minute = 10;
+    printf("残り%02d時間%02d分", $limit_hour, $limit_minute);
+
+    echo '<br>';
+    $limit_hour = 3;
+    $limit_minute = 5;
+    printf("残り%02d時間%02d分", $limit_hour, $limit_minute);
+    
+?>
+
+
---
+++ 2-13/word13.php
@@ -0,0 +1,14 @@
+<?php
+echo "1. PostgreSQL・Oracle SQL"."<br>";
+echo "PostgreSQL（ポストグレスキューエル）はオープンソースのリレーショナルデータベース管理システム（RDBMS）。Linux、macOSといったUNIX系OSはもちろんのこと、Windowsにも対応している。すべてのソースは公開されており、用途を問わず無料で利用できる。"."<br>";
+echo "Oracle SQLはOracle社が開発した関係データベース管理システムのこと。2次元の表形式でデータを表し、SQL言語によって検索や入力、消去などの処理を行える。
+
+"."<br>";
+echo '<br>';
+echo "2. TortoiseGit・TortoiseSVN"."<br>";
+echo "TortoiseGitとは、Gitをより扱いやすくするために開発されたWindows用ソフトウェア。無料で扱うことができる。"."<br>";
+echo "TortoiseSVN は、Subversion のクライアントで、Windowsのシェルエクステンションとして機能する。専用のGUIを備えており、CUI の Subversion より、直感的で簡単に扱うことが出来る。Subversion はクライアント/サーバ型の集中型バージョン管理システム。"."<br>";
+echo '<br>';
+echo "3. 外部設計・内部設計"."<br>";
+echo "外部設計とは、要件定義をもとにユーザーインターフェースを設計する工程。対して内部設計では機能やプログラムの仕様や処理内容などを設計し、外部設計と実装の間の工程となる。"."<br>";
+?>
