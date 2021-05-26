021a03018a5525b77b1153e51ce837e3a11c2997
miyakeiko <79643269+miyakeiko@users.noreply.github.com>
2021-05-26 18:56:43 +0900

    ファイルのデータを読み書きする　課題提出
    課題提出

---
+++ 2-16/word16.php
@@ -0,0 +1,20 @@
+<?php
+echo "1. CakePHPの2系・3系の違い"."<br>";
+echo "CakePHP2はComposerでのインストールに対応していなかったことに対して、CakePHP3はComposerで簡単にインストールすることができるようになりました。"."<br>";
+echo "CakePHP2のQuelyBuilderでは多次元配列をつかい複雑だったものが、CakePHP3では簡単に使えるように変更されています。"."<br>";
+
+echo '<br>';
+echo '<br>';
+
+echo "2. LAMP"."<br>";
+echo "データベースを利用したWebアプリケーションを開発・運用するのに適した、人気の高いオープンソースソフトウェアの組み合わせの一つで、OSの「Linux」、Webサーバの「Apache」、データベースの「MySQL」（後にMariaDBも選択肢に追加された）、プログラミング言語および実行環境の「PHP」「Perl」「Python」（のいずれか）の頭文字を繋いだもの。"."<br>";
+
+echo '<br>';
+echo '<br>';
+
+echo "3. AWS"."<br>";
+echo "Amazon Web Servicesの略。 Amazon.comの子会社であるAmazon Web Servicesによって提供されているクラウドコンピューティングサービス（ウェブサービス）。"."<br>";
+
+echo '<br>';
+echo '<br>';
+?>
